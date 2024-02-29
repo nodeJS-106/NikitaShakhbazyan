06fbd353759f1fa2077e47231b89799bb0ff72b6
nickshakh <shakhbazyann@gmail.com>
2024-02-29 20:01:32 +0400

    [fix] - added getUsers method.

--- hw4/script.js
+++ hw4/script.js
@@ -33,7 +33,19 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end('Data file has been deleted from file');
         })
-    } else {
+    } else if (req.method === "GET" && req.url === '/getUsers') {
+        fs.readFile('data.json', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('Error reading file');
+            } else {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(data);
+            }
+        });
+    }    
+    else {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.end('Not Found');
     }
