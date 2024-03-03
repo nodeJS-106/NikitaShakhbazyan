8d48d96ce0a97539fb1cc5aff8e0bb2e63a6fcd9
nickshakh <shakhbazyann@gmail.com>
2024-03-03 19:59:18 +0400

    made first configuration

--- hw5/server.js
+++ hw5/server.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+dotenv.config();
 
-app.get('/app',)
-app.post('/app',)
-app.put('/app',)
-app.delete('/app',)
+const port = process.env.PORT || 3006
+
+app.get('/app',(req,res)=>{
+    res.status(200).json({message : "Getting data"})
+})
+app.get('/app/:id',(req,res)=>{
+    res.status(200).json({message : `Getting data from user with ${req.params.id} id` })
+})
+app.post('/app',(req,res) => {
+    res.status(201).json({message:"Creating data"})
+})
+app.put('/app/:id',(req,res)=> {
+    res.status(200).json({message : `Changing data of user with id : ${req.params.id}`})
+})
+app.delete('/app/:id',(req,res) => {
+    res.status(200).json({message : `Deeting data of user with id : ${req.params.id}`})
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log('Server is on')
+}) 
